be49e9acc9eba2217c182afa28df72762ee28c6a
afantino951 <andrew.fantino.951@gmail.com>
2023-11-20 19:34:06 -0800

    Scale with distance script for sign object

--- BusStopARDemo/Assets/MyScripts/ScaleWithDistance.cs
+++ BusStopARDemo/Assets/MyScripts/ScaleWithDistance.cs
@@ -3,24 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using Oculus.Interaction.Input;
 using Oculus.Interaction;
+using Meta.WitAi.Utilities;
 public class ScaleWithDistance : MonoBehaviour
 {
     [SerializeField]
     private GameObject _sourceGameObject;
 
-    [SerializeField, Interface(typeof(IOVRCameraRigRef))]
-    private UnityEngine.Object _cameraRigRef;
+    [SerializeField]
+    private GameObject _cameraRigRef;
 
     [SerializeField]
     private GameObjectActiveState _objectActiveState;
 
 
     private float distance;    
+    private float scaleFactor = 0.01f;
 
     // Start is called before the first frame update
     void Start()
     {
-        distance = Vector3.Distance(_sourceGameObject.transform.position, _cameraRigRef.transform.position);
+        distance = getDistance(_sourceGameObject, _cameraRigRef);
+        Debug.Log("Distance: " + distance);
     }
 
     // Update is called once per frame
@@ -28,8 +31,20 @@ public class ScaleWithDistance : MonoBehaviour
     {
         if (_objectActiveState.Active) 
         {
-            distance = Vector3.Distance(_sourceGameObject.transform.position, _cameraRigRef.transform.position);
+            distance = getDistance(_sourceGameObject, _cameraRigRef);
+            Debug.Log(distance);
+
+            if (distance < 1.0f) 
+            {
+                _sourceGameObject.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+            }
+            // _sourceGameObject.transform.localScale *= scaleFactor;
             
         }
+    }
+
+    private static float getDistance(GameObject obj1, GameObject obj2) 
+    {
+        return Vector3.Distance(obj1.transform.position, obj2.transform.position);
     }
 }
